a74cd0fb2cea2af6cf15fb69fe81fd38e7d868a6
SiddhantBhosale05 <siddhant.bhosale05@gmail.com>
2024-08-02 11:36:23 +0530

    Add files via upload

---
+++ Hello loop.cpp
@@ -0,0 +1,25 @@
+//Name:Siddhant Kiran Bhosale
+//PRN:23070123038
+//Aim:Implemention of Decision Making Statements Loops to print Hello Five times 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { 
+    string a;
+    a = "Hello";
+for(int i=0;i<5;i++)
+
+{cout << "Hello"<< endl;}
+
+    return 0;
+}
+
+
+/*Output:
+Hello
+Hello
+Hello
+Hello
+Hello*/
