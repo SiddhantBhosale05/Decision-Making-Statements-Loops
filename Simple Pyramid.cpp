063b306eca1a978e72024b4b789b1d80442eee78
Siddhant Bhosale <siddhant.bhosale05@gmail.com>
2024-10-12 16:08:17 +0530

    Simple Pyramid.cpp

--- Simple Pyramid.cpp
+++ Simple Pyramid.cpp
@@ -1,6 +1,6 @@
 //Name:Siddhant Kiran Bhosale
 //PRN:23070123038
-//Aim:Implemention of Decision Making Statements Loops to print Simple Pyramid
+//Aim:Implementation of Decision Making Statements Loops to print Simple Pyramid
 
 #include <iostream>
 using namespace std;
