ee6963eca1e6cf6bd1548df93a898926c551bba0
Siddhant Bhosale <siddhant.bhosale05@gmail.com>
2024-10-12 16:05:32 +0530

    Flipped Simple Pyramid.cpp

--- Flipped Simple Pyramid.cpp
+++ Flipped Simple Pyramid.cpp
@@ -1,6 +1,6 @@
 //Name:Siddhant Kiran Bhosale
 //PRN:23070123038
-//Aim:Implemention of Decision Making Statements Loops to print Flipped Simple Pyramid
+//Aim:Implementation of Decision Making Statements Loops to print Flipped Simple Pyramid
 
 #include <iostream>
 using namespace std;
